951638270bdc1d39903dd0c5eea04e98441e003b
Adithya Kamath <adithyakamath96@gmail.com>
2020-01-15 20:34:23 +0530

    Added Expenses summary

---
+++ src/tests/components/ExpensesSummary.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {ExpensesSummary} from '../../components/ExpensesSummary';
+
+test('Should correctly render expenses summary with 1 expense',()=>{
+    const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={235}/>);
+    expect(wrapper).toMatchSnapshot();
+});
+test('Should correctly render expenses summary with multiple expenses',()=>{
+    const wrapper = shallow(<ExpensesSummary expenseCount={23} expensesTotal={71717277280}/>);
+    expect(wrapper).toMatchSnapshot();
+});
