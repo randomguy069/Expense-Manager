5888fe492cd98a38bfff5600462aae128f7639b5
Adithya Kamath <adithyakamath96@gmail.com>
2020-01-22 02:05:45 +0530

    Setup expenses

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if(process.env.NODE_ENV === 'test'){
-  require('dotenv').config({path: '.env.test'});
-}else if(process.env.NODE_ENV === 'development'){
-  require('dotenv').config({path: '.env.development'});
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
 }
 
 module.exports = (env) => {
@@ -54,7 +54,6 @@ module.exports = (env) => {
         'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
         'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
         'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
-
       })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
