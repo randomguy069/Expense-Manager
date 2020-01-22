2e0a73cc981e3ddbfd7bc810e83fc354b63f4729
Adithya Kamath <adithyakamath96@gmail.com>
2020-01-23 01:07:15 +0530

    Changes to removing an expense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,20 +1,23 @@
 import uuid from 'uuid';
 import database from '../firebase/firebase';
+
 // ADD_EXPENSE
 export const addExpense = (expense) => ({
   type: 'ADD_EXPENSE',
   expense
 });
-export const startAddExpense = (expenseData = {})=>{
-  return (dispatch)=>{
+
+export const startAddExpense = (expenseData = {}) => {
+  return (dispatch) => {
     const {
       description = '',
       note = '',
       amount = 0,
       createdAt = 0
     } = expenseData;
-    const expense = {description,note,amount,createdAt};
-    return database.ref('expenses').push(expense).then((ref)=>{
+    const expense = { description, note, amount, createdAt };
+
+    return database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
         ...expense
@@ -22,11 +25,21 @@ export const startAddExpense = (expenseData = {})=>{
     });
   };
 };
+
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
   id
 });
+
+export const startRemoveExpense = ({id}= {})=>{
+  return (dispatch)=>{
+    return database.ref(`expenses/${id}`).remove().then(()=>{
+      dispatch(removeExpense({id}));
+    });
+  };
+};
+
 
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
@@ -35,8 +48,7 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
-//SET_EXPENSES
-
+// SET_EXPENSES
 export const setExpenses = (expenses) => ({
   type: 'SET_EXPENSES',
   expenses
