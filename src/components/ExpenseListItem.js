636858041107d3b1ce4853e12c89725e5a1e2069
Adithya Kamath <adithyakamath96@gmail.com>
2020-01-30 02:23:58 +0530

    Added styles for Expense page, Expense form

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -5,14 +5,16 @@ import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
 
-  <div>
-    <Link to={`/edit/${id}`}>
+
+  <Link className="list-item" to={`/edit/${id}`}>
+    <div>
       <h3>{description}</h3>
-    </Link>
-    <p>{numeral(amount/100).format("$0,0.00").replace("$","₹")}
-     - 
-    {moment(createdAt).format('MMMM Do, YYYY')}</p>
-  </div>
+      <span>{moment(createdAt).format('MMMM Do, YYYY')}</span>
+    </div>
+    <h3> {numeral(amount / 100).format("$0,0.00").replace("$", "₹")}</h3>
+  </Link>
+
+
 );
 
 export default ExpenseListItem;
