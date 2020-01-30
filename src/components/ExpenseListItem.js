d759062d0225811514f4f260977e1c3103db31f7
Adithya Kamath <adithyakamath96@gmail.com>
2020-01-31 01:53:19 +0530

    App phase 1 complete

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -8,13 +8,14 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
 
   <Link className="list-item" to={`/edit/${id}`}>
     <div>
-      <h3>{description}</h3>
-      <span>{moment(createdAt).format('MMMM Do, YYYY')}</span>
+      <h3 className="list-item__title">{description}</h3>
+      <span className="list-item__sub-title">{moment(createdAt).format('MMMM Do, YYYY')}</span>
     </div>
-    <h3> {numeral(amount / 100).format("$0,0.00").replace("$", "₹")}</h3>
+    <h3 className="list-item__data"> {numeral(amount / 100).format("$0,0.00").replace("$", "₹")}</h3>
   </Link>
 
 
 );
+
 
 export default ExpenseListItem;
